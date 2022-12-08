9c84d5d9ea8ad88d23f86e55656992822f93f75c
Joshuanluo <joshua.chuan.luo@gmail.com>
2022-12-08 14:33:55 +1100

    ignore env

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { db } from "./Firebase-config";
+
+const DashBoard = () => {
+	const [mail, setMail] = useState();
+	const mailCollectionRef = collection(db, "Mailing-List");
+	useEffect(() => {
+		const getMail = async () => {
+			const data = await getDocs(mailCollectionRef);
+			console.log(data);
+            setMail(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+			console.log(mail);
+		};
+        getMail();
+	},[]);
+	return <div>DashBoard</div>;
+};
+
+export default DashBoard;
--- src/components/Firebase-config.jsx
+++ src/components/Firebase-config.jsx
@@ -1,14 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_APIDOMAIN,
+  projectId: "storm-chaser-colab",
+  storageBucket: "storm-chaser-colab.appspot.com",
+  messagingSenderId: "291106799479",
+  appId: "1:291106799479:web:3dd114e3ef036bd75fab72"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 export const db =getFirestore(app);
