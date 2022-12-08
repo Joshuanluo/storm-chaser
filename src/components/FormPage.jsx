af2d2e4e1be9db00e268d97e8db3842bcb7ee193
Michael Anderson <manderson3568@outlook.com.au>
2022-12-09 09:50:47 +1100

    added envelope icon

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -2,48 +2,72 @@ import React, { useState } from "react";
 import { db } from "./Firebase-config";
 import { addDoc, collection, Timestamp } from "firebase/firestore";
 import "../formpage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons";
 
 const FormPage = () => {
-	const [firstName, setFirstName] = useState("");
-	const [lastName, setLastName] = useState("");
-	const [email, setEmail] = useState("");
-	const mailsCollectionRef = collection(db, "Mailing-List");
-	const _handleSubmit = async (event) => {
-		event.preventDefault();
-		await addDoc(mailsCollectionRef, { first_name: firstName, last_name: lastName, email: email, time: Timestamp.fromDate(new Date()) });
-	};
+  const element = <FontAwesomeIcon icon={faEnvelopeOpenText} size="6x" />;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const mailsCollectionRef = collection(db, "Mailing-List");
+  const _handleSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(mailsCollectionRef, {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      time: Timestamp.fromDate(new Date()),
+    });
+  };
 
-	return (
-		<div className="formpage">
-			<div className="formpage_title">GET OUR WEEKLY 1-MINUTE NEWSLETTER</div>
-			<div className="formpage_content">
-				Join thousands of readers and receive weekly emails with warnings for QLD, NSW, ACT & VIC with Tropical Cyclone map Australia wide!
-			</div>
+  return (
+    <div className="formpage">
+      {element}
+      <div className="formpage_title">GET OUR WEEKLY 1-MINUTE NEWSLETTER</div>
+      <div className="formpage_content">
+        Join thousands of readers and receive weekly emails with warnings for
+        QLD, NSW, ACT & VIC with Tropical Cyclone map Australia wide!
+      </div>
 
-			<form onSubmit={_handleSubmit}>
-				<label className="infolabel1">
-					First Name:
-					<input
-						className="infoinput"
-						required
-						type="text"
-						placeholder="First Name"
-						onChange={(e) => setFirstName(e.target.value)}
-						value={firstName}
-					/>
-				</label>
-				<label className="infolabel2">
-					Last Name:
-					<input className="infoinput" required type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} value={lastName} />
-				</label>
-				<label className="infolabel3">
-					Email address:
-					<input className="infoinput" required type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} />
-				</label>
-				<button className="infobtn">Submit</button>
-			</form>
-		</div>
-	);
+      <form onSubmit={_handleSubmit}>
+        <label className="infolabel1">
+          First Name:
+          <input
+            className="infoinput"
+            required
+            type="text"
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+          />
+        </label>
+        <label className="infolabel2">
+          Last Name:
+          <input
+            className="infoinput"
+            required
+            type="text"
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+        </label>
+        <label className="infolabel3">
+          Email address:
+          <input
+            className="infoinput"
+            required
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <button className="infobtn">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormPage;
