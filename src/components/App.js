03da43acf4a0857f04595b7100c72d6dda87eb61
Michael Anderson <manderson3568@outlook.com.au>
2022-12-09 09:08:31 +1100

    homepage skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import logo from "../logo.svg";
 import "../App.css";
 import DashBoard from "./DashBoard";
 import DisplayNavbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <DisplayNavbar />
-      <DashBoard />
+      {/* <DashBoard /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Higgins Storm Chasing</h1>
+
+      <h2>Forcasts-Warnings-Observations</h2>
+
+      <h3>Advanced Weather Service</h3>
+      <p>Available for QLD, NSW, ACT & VIC!</p>
+
+      <p>Every day you will "know" the weather...</p>
+
+      <button className="blueSubscribe">
+        Subscribe to our Weather Service
+      </button>
+      <button className="yellowSubscribe">Get our 1-minute newsletter</button>
+    </div>
+  );
+};
+export default Home;
