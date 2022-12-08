30c311de1349ad69c5e4db65f75b7f47fcef0db1
Joshuanluo <joshua.chuan.luo@gmail.com>
2022-12-08 20:24:49 +1100

    add form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import logo from "../logo.svg";
 import "../App.css";
 import DashBoard from "./DashBoard";
+import FormPage from "./FormPage";
 
 function App() {
   return (
     <div className="App">
+
       <DashBoard />
+      <hr />
+      <FormPage />
+      
+      
     </div>
   );
 }
--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,25 +1,34 @@
-import React from "react";
-import {Button, Form} from 'react-bootstrap';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const FormPage = () => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+
+	const _handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(firstName, lastName, email);
+	};
+
 	return (
 		<div>
-			<Form>
-				<Form.Group className="mb-3" >
+			<Form onSubmit={_handleSubmit}>
+				<Form.Group className="mb-3">
 					<Form.Label>First Name</Form.Label>
-					<Form.Control type="text" placeholder="First Name" onChange={(e)=>e.target.value}/>
+					<Form.Control type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
 				</Form.Group>
 
 				<Form.Group className="mb-3">
 					<Form.Label>Last Name</Form.Label>
-					<Form.Control type="text" placeholder="Last Name" onChange={(e)=>e.target.value}/>
+					<Form.Control type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
 				</Form.Group>
-				
+
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" onChange={(e)=>e.target.value}/>
+					<Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
 				</Form.Group>
-                
+
 				<Button variant="primary" type="submit">
 					Submit
 				</Button>
