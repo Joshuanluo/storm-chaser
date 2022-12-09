6e57042e84525158740c74e39d4e9e48297e2e96
Joshuanluo <joshua.chuan.luo@gmail.com>
2022-12-09 15:30:11 +1100

    adjust dashboard css

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./Firebase-config";
 import "../dashboard.css";
+import Table from "react-bootstrap/Table";
 
 const DashBoard = ({ toHome }) => {
 	const [mails, setMails] = useState([]);
@@ -17,21 +18,29 @@ const DashBoard = ({ toHome }) => {
 
 	return (
 		<div className="dash_main">
-			<h2>this is dashboard</h2>
-			<table>
-				<tr>
-					<td>Name</td>
-					<td>Email</td>
-				</tr>
-				{mails.map((mail) => {
-					return ( 
-						<tr key={mail.id}>
-							<td>{mail.first_name} {mail.last_name}</td>
-							<td>{mail.email}</td>
+			<div className="dash_table">
+      <h1>Dashboard</h1>
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<td>Name</td>
+							<td>Email</td>
 						</tr>
-					);
-				})}
-			</table>
+					</thead>
+					<tbody>
+						{mails.map((mail) => {
+							return (
+								<tr key={mail.id}>
+									<td>
+										{mail.first_name} {mail.last_name}
+									</td>
+									<td>{mail.email}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			</div>
 		</div>
 	);
 };
